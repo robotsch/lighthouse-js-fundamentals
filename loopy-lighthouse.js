c68b7f97b7d66566c2f7a757e4d7f7970e7d9d4f
Christian Humble <christianhumble22@gmail.com>
2022-01-28 18:52:37 +0000

    Add loopy-lighthouse.js and hello.js

---
+++ loopy-lighthouse.js
@@ -0,0 +1,13 @@
+let start = 100
+
+for(let i = start; i <= 200; i++){
+  if(i % 3 === 0 && i % 4 === 0) {
+    console.log('LoopyLighthouse')
+  } else if(i % 3 === 0) {
+    console.log('Loopy')
+  } else if (i % 4 === 0) {
+    console.log('Lighthouse')
+  } else {
+    console.log(i)
+  }
+}
