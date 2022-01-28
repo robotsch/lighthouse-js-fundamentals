695541b6e1afd6015d02a2f419e640b216a5ee6a
Christian Humble <christianhumble22@gmail.com>
2022-01-28 18:55:47 +0000

    Add even.js

--- hello.js
+++ hello.js
@@ -3,3 +3,9 @@ const sayHello  = function (name) {
 }
 
 sayHello('Christian');
+
+const returnSayHello  = function (name) {
+  return `Hello, ${name}`
+}
+const greeting = returnSayHello('Christian');
+console.log(greeting);
